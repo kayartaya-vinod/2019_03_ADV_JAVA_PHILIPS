aa62eb37d7601f771b9e4e6180be08aa07d8ab95
kayartaya.vinod <kayartaya.vinod@gmail.com>
2019-03-20 16:59:11 +0530

    🇮🇳

--- EclipseWorkspace/springboot-training/src/main/java/com/philips/springboottraining/ProductResource.java
+++ EclipseWorkspace/springboot-training/src/main/java/com/philips/springboottraining/ProductResource.java
@@ -5,12 +5,14 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RequestMapping(path = "/api/products", produces = "application/json")
 @RestController
 public class ProductResource {
--- products-manager-app/src/app/product-list/product-list.component.ts
+++ products-manager-app/src/app/product-list/product-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const url = 'http://localhost:8080/spring-training/api/products/';
+const url = 'http://localhost:8080/api/products/';
 
 
 @Component({
